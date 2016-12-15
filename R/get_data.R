ed84b28565bb953ab76ac00f940db5ced19112a0
jobonaf <midable@gmail.com>
2016-12-15 22:10:32 +0100

    get_data.R

--- R/get_data.R
+++ R/get_data.R
@@ -1,4 +1,70 @@
-get_ftp <- function(){}
-get_http <- function(){}
-get_ssh <- function(){}
-get_data <- function(){}
+get_ftp <- function(config,FileIn){
+  source(config)
+  command <- paste("ftp -n ",Addr,"<<EOF\n",
+                   "user ",Usr," ",Pwd,"\n",
+                   "cd ./",Path,"\n",
+                   "get ",FileIn,"\n",
+                   "bye\nEOF",
+                   sep="")
+  system(command)
+  check <- file.exists(FileIn)
+  return(check)
+}
+
+get_http <- function(config,FileIn,proxyconfig){
+  source(config)
+  source(proxyconfig)
+  Sys.setenv(http_proxy=paste0("http://",proxy_usr,":",proxy_pwd,"@",proxy_addr,":",proxy_port,"/"))
+  command <- paste0("wget ",Addr,"/",Path,"/",FileIn,
+                    " --proxy-user=",proxy_usr," --proxy-password=",proxy_pwd,
+                    " --no-check-certificate")
+  system(command)
+  check <- file.exists(FileIn)
+  return(check)}
+
+get_ssh <- function(config,FileIn){
+  source(config)
+  command <- paste("sshpass -p '",Pwd,
+                   "' scp ",Usr,"@",Addr,":",Path,"/",FileIn," .",
+                   sep="")
+  system(command)
+  check <- file.exists(FileIn)
+  return(check)
+}
+
+get_local <- function(config,FileIn){
+  source(config)
+  command <- paste("cp ",Path,"/",FileIn," .",
+                   sep="")
+  system(command)
+  check <- file.exists(FileIn)
+  return(check)
+}
+
+get_file <- function(config,FileIn){
+  source(config)
+  dd <- ifelse(!is.na(Datefmt) && nchar(Datefmt)>0,
+               format(as.POSIXct(day),
+                      format=Datefmt),
+               "")
+  FileIn <- paste(Before, dd, After,sep="")
+  if(file.exists(FileIn)) file.remove(FileIn)
+
+  check <- switch(Type,
+                  ftp  =get_ftp  (config,FileIn),
+                  http =get_http (config,FileIn,proxyconfig),
+                  ssh  =get_ssh  (config,FileIn),
+                  local=get_local(config,FileIn))  
+    
+  if(check) {
+    FileOut <- paste(Source,Content,
+                     format(as.POSIXct(day),format="%Y%m%d"),
+                     "dat",
+                     sep=".")
+    file.rename(FileIn, FileOut)
+    cat(paste0("rinominato ",FileIn, " in ",FileOut,"\n"))
+  } else {
+    FileOut <- ""
+  }
+  return(FileOut)
+}
--- R/prepare_data.R
+++ R/prepare_data.R
@@ -1,2 +1,4 @@
-prepare_data <- function(){}
-meld_data <- function(){}
+read_data <- function(){}
+read_info <- function(){}
+data2db <- function(){}
+info2db <- function(){}

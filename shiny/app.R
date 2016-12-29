1a67e49e73f9eb1752774d2906d3ad27729a476b
jobonaf <midable@gmail.com>
2016-12-29 23:36:35 +0100

    shiny app: data table, timeseries plot, peaks detection

--- shiny/app.R
+++ shiny/app.R
@@ -1,8 +1,8 @@
 #packages
 suppressMessages({
   library(shiny)
-  library(shinyBS)
-  library(shinyjs)
+#  library(shinyBS)
+#  library(shinyjs)
   library(leaflet)
   library(DT)
   library(rgdal)
@@ -18,30 +18,6 @@ suppressMessages({
 # credentials
 source("/home/giovanni/R/projects/calicantus/config/ui_credentials.R")
 
-# load data
-load_data <- function(lday=Sys.Date()-1,fday=lday-30) {
-  dd <- as.character(seq.Date(fday,lday,"1 day"))
-  ff <- NULL
-  for(d in dd) {
-    ff <- c(ff,system(paste0("ls /home/giovanni/R/projects/calicantus/data/obs-data/",
-                             format(as.Date(d),"%Y/%m/%d/%Y%m%d_*.rda 2>/dev/null")),
-                      intern=TRUE))
-  }
-  Dat <- NULL
-  for (f in ff) {
-    cat(f,sep="\n")
-    load(f)
-    Dat <- bind_rows(Dat,dat)
-  }
-  Dat$Value <- round(Dat$Value)
-  return(Dat)
-}
-Dat <- load_data()
-na.omit(Dat) -> Dat
-lon0 <- (min(Dat$Lon)+max(Dat$Lon))/2
-lat0 <- (min(Dat$Lat)+max(Dat$Lat))/2
-cat(paste(lon0,lat0))
-
 # parameters
 bb <- c(0,25,50,75,100,300)
 cc <- c("steelblue","olivedrab","orange","red","purple")
@@ -51,6 +27,28 @@ ui_map <- fluidPage(leafletOutput("Map", width="100%", height="600px")
           ,absolutePanel(id="controls", top=100, right=100, height=200, width=100
                          ,dateInput("day", "", Sys.Date()-1)
           )
+)
+
+# UI: data table
+ui_data <- sidebarLayout(
+  sidebarPanel(dateRangeInput("daterange","period of analysis",
+                              start=Sys.Date()-10,
+                              end=Sys.Date()-1,
+                              max = Sys.Date()-1),
+               width=3),
+  mainPanel(dataTableOutput("df"))
+)
+
+# UI: time series
+ui_ts <- sidebarLayout(
+  sidebarPanel(selectInput("colorby","color by",c("data source"="Source","same color"="same")),
+               selectInput("splitby","split by",c("data source"="Source","don't split"="none")),
+               selectInput("geom_type","plot type",c("boxplot","violin","jitter")),
+               selectInput("emphasis","emphasis",c("period max","daily max","data source max","none")),
+               sliderInput("howmany","how many peaks do you want to emphasize?",1,5,1,1,ticks = FALSE),
+               checkboxInput("labels","peaks labelled with values?",FALSE),
+               width=3),
+  mainPanel(plotOutput("ts"))
 )
 
 # UI: about
@@ -92,7 +90,9 @@ ui_login <- function(){
   )}
 
 # UI: webpage with tabs
-ui_main <- function(){tagList(tabPanel("map",ui_map),
+ui_main <- function(){tagList(tabPanel("data",ui_data),
+                              tabPanel("map",ui_map),
+                              tabPanel("timeseries",ui_ts),
                               tabPanel("about",ui_about))}
 ui <- (htmlOutput("page"))
 
@@ -119,25 +119,89 @@ server <- function(input, output, session) {
     # main page
     if (USER$Logged) {
       output$page <- renderUI({
-        div(class="outer",do.call(navbarPage,c(inverse=TRUE,title = "calicantus",ui_main())))
+        div(class="outer",do.call(navbarPage,c(inverse=TRUE,title = "[calicantus]",ui_main())))
       })
       print(ui)
     }
   })
   
+  # load data
+  dataOfPeriod <- reactive({
+    dd <- as.character(seq.Date(input$daterange[1],input$daterange[2],"1 day"))
+    ff <- NULL
+    for(d in dd) {
+      ff <- c(ff,system(paste0("ls /home/giovanni/R/projects/calicantus/data/obs-data/",
+                               format(as.Date(d),"%Y/%m/%d/%Y%m%d_*.rda 2>/dev/null")),
+                        intern=TRUE))
+    }
+    Dat <- NULL
+    nf <- length(ff)
+    for (i in 1:nf) {
+      load(ff[i])
+      Dat <- bind_rows(Dat,dat)
+    }
+    Dat$Value <- round(Dat$Value)
+    return(Dat)
+  })
   
-  # data selection
-  selectedData <- reactive({
+  # data table
+  output$df <- renderDataTable({
+    dataOfPeriod()
+  }, options = list(pageLength = 20))
+  
+  # peaks detection
+  dataWithPeaks <- reactive({
+    dataOfPeriod() %>% mutate(Station=paste0(Name," (",Source,")")) -> Dat
+    if(input$emphasis=="none") {
+      Dat$Emphasis <- Dat$Peak <- NA
+    } else {
+      if(input$emphasis=="period max") Dat %>% group_by(Pollutant) -> Dat  
+      if(input$emphasis=="daily max") Dat %>% group_by(Pollutant,Day) -> Dat  
+      if(input$emphasis=="data source max") Dat %>% group_by(Pollutant,Source) -> Dat  
+      Dat %>% mutate(Rank=1+n()-rank(Value,na.last=FALSE), Peak=(Rank<=input$howmany)) %>% left_join(.,Dat) -> Dat
+      Dat %>% group_by(Pollutant,Name,Source) %>% mutate(Emphasis=max(Peak,na.rm=T)) %>% left_join(.,Dat) -> Dat
+    }
+    Dat$Emphasis[!Dat$Emphasis] <- NA
+    Dat$Peak[!Dat$Peak] <- NA
+    Dat
+  })
+  
+  # plot time series
+  output$ts <- renderPlot({
+    Aes <- aes_string(x="Day", y="Value", group="Day")
+    if(input$colorby!="same") Aes <- modifyList(Aes, aes_string(col=input$colorby))
+    pl <- ggplot(data = dataWithPeaks(), Aes)
+    geom_ <- get(paste0("geom_",input$geom_type))
+    pl <- pl + theme_bw() + geom_(width=0.4)
+    if(input$splitby!="none") pl <- pl + facet_wrap(input$splitby) 
+    if(input$emphasis!="none") pl <- pl +
+      geom_line(aes(x=as.numeric(ordered(Day))*Emphasis, y=Value*Emphasis, group=Station)) + 
+      geom_point(aes(x=as.numeric(ordered(Day))[Peak], y=Value[Peak]), shape=21, fill="white", size=5) +
+      geom_point(aes(x=as.numeric(ordered(Day))[Peak], y=Value[Peak], shape=Station[Peak], group=Station[Peak]), size=2.5) +
+      scale_shape_manual(values=c(65:90,97:122))
+    if(input$labels) pl <- pl +
+      geom_text(aes(x=as.numeric(ordered(Day))[Peak]-0.2, y=Value[Peak], label=Value), hjust=1, vjust=0.3)
+    pl <- pl + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+    pl
+  },height = function(){ifelse(input$splitby=="none",600,900)})
+
+  # data selection for the map (single day)
+  dataOfDay <- reactive({
+    Dat <- dataOfPeriod()
     Dat[Dat$Day==as.character(input$day),]
   })
   
   # daily map
   output$Map <- renderLeaflet({
+    na.omit(dataOfDay()) -> Dat
+    lon0 <- (min(Dat$Lon)+max(Dat$Lon))/2
+    lat0 <- (min(Dat$Lat)+max(Dat$Lat))/2
+    
     leaflet() %>% setView(lon0, lat0, 6) %>% addTiles()  %>% addProviderTiles("CartoDB.Positron") %>%
-      addCircleMarkers(data=selectedData()
+      addCircleMarkers(data=Dat
                        ,lng=~Lon, lat=~Lat
                        ,radius=6 
-                       ,color=as.character(cut(selectedData()$Value,
+                       ,color=as.character(cut(Dat$Value,
                                                breaks=bb,
                                                labels=cc))
                        ,stroke=FALSE, fillOpacity=0.5

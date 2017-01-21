20644bbff193587c66c9b3fb7d1af1c1b76b5585
jobonaf <midable@gmail.com>
2017-01-21 20:15:30 +0100

    templates for users permissions

---
+++ config/template.config_users.R
@@ -0,0 +1,7 @@
+if(input$Usr=="usr1") {
+  auth_sources <- c("EPA-Botswana","EPA-Namibia")
+}else if(input$Usr=="usr2"){
+  auth_sources <- c("EPA-Botswana","EPA-Kenya")
+}else{
+  auth_sources <- NULL
+}
---
+++ config/template.ui_credentials.R
@@ -0,0 +1,9 @@
+ui_usr <- vector()
+ui_pwd <- vector()
+
+## 1
+ui_usr[1] <- "usr1"
+ui_pwd[1] <- "123456"
+## 2
+ui_usr[2] <- "usr2"
+ui_pwd[2] <- "abcdefg"

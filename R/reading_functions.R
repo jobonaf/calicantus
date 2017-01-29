5c700272eb1c34ad359385de3a70a048ea698cd6
jobonaf <midable@gmail.com>
2017-01-29 22:30:36 +0100

    read.ArpaLazio

--- R/reading_functions.R
+++ R/reading_functions.R
@@ -33,3 +33,32 @@ read.ArpaT <- function(file,day) {
   dat <- dat[which(dat$DATA_OSSERVAZIONE==Day),]
   return(dat)
 }
+
+read.ArpaLazio <- function(file,sep,day) {
+  dat <- NULL
+  for(ff in file) {
+    dd <- try(read.table(ff,head=T,check.names = F,stringsAsFactors = FALSE))
+    if(class(dd)!="try-error") {
+      if(is.null(dat)) {
+        dat<-dd
+      } else {
+        dat <- merge(x=dat,y=dd,by=c("anno","jd"),all=T)
+      }
+    }
+  }
+  idx <- which(as.character(dat$anno)==format(as.POSIXct(day),"%Y") & dat$jd==(as.POSIXlt(day)$yday+1))
+  out <- data.frame(ID=as.character(colnames(dat)[-1:-2]), Valore=c(unlist(dat[idx[1],][-1:-2])),stringsAsFactors = F)
+  return(out)
+}
+
+
+read.ArpaPuglia <- function(file,sep,day) {
+  dat <- read.table(file,sep = sep,blank.lines.skip = T,fill = T,stringsAsFactors = F)
+  dat[,1] <- stringr::str_trim(dat[,1])
+  dat[,colSums(!is.na(dat))==0] <- NULL
+  hdr <- dat[as.character(dat[,1])=="NomeCentralina",][1,]
+  colnames(dat) <- stringr::str_trim(as.character(hdr))
+  dat$Valore <- as.numeric(as.character(dat$Valore))
+  out <- dat[which(!is.na(dat$Valore)),]
+  return(out)
+}
